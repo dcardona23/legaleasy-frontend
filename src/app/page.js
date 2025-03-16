ce5b34f01245157c4deb027e99a808c5414f7d58
Danielle Cardona <danielle.cardona23@gmail.com>
2025-03-15 20:56:50 -0600

    direct page.js to fetch from backend

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ const Home = () => {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch('https://your-rails-api-app.herokuapp.com/api/v1/hello')
+    fetch('https://legaleasy-backend.herokuapp.com/api/v1/hello')
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   }, []);
