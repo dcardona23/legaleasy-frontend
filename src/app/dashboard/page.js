9e7607258813fe160a889fb81b11b5fd518ec6fd
Danielle Cardona <danielle.cardona23@gmail.com>
2025-04-08 17:10:54 -0600

    feat/add dashboard with imports for dashboard components

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,5 +1,19 @@
-import Sidebar from "@/app/components/Sidebar";
+import TopNavBar from "@/app/components/NavBar";
+import DashboardFooter from "../components/DashboardFooter";
+import HeroSection from "../components/HeroSection";
+import PopularForms from "../components/PopularForms";
+import QuickActions from "../components/QuickActions";
 
-export default function Dashboard() {
-  return <Sidebar />;
+function Dashboard() {
+  return (
+    <div className="min-vh-100 d-flex flex-column">
+      <TopNavBar />
+      <HeroSection />
+      <PopularForms />
+      <QuickActions />
+      <DashboardFooter />
+    </div>
+  );
 }
+
+export default Dashboard;
